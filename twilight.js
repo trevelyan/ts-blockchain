db8494953fde6de4d6ba6b8296d35e30a8fe5326
David Lancashire <david@popupchinese.com>
2019-06-18 22:39:37 +0900

    update for cards

--- twilight.js
+++ twilight.js
@@ -36,7 +36,7 @@ function Twilight(app) {
   // hardcodes the hands for each player (editable) during
   // placement for easier interactive card testing.
   //
-  this.is_testing = 0;
+  this.is_testing = 1;
 
 
   //
@@ -1773,7 +1773,7 @@ console.log("resolving earlier: " + this.game.queue[z]);
 
         if (this.is_testing == 1) {
           if (this.game.player == 1) {
-            this.game.deck[0].hand = ["handshake","quagmire", "asknot", "junta", "che","degaulle","nato","naziscientist","missileenvy"];
+            this.game.deck[0].hand = ["cubanmissile","quagmire", "asknot", "junta", "che","degaulle","nato","naziscientist","missileenvy"];
           } else {
             this.game.deck[0].hand = ["u2","wwby","unintervention","onesmallstep","summit","lonegunman","oas","nasser","sadat"];
           }
@@ -3018,7 +3018,9 @@ Twilight.prototype.playMove = function playMove(msg) {
   //
   let scoring_cards_available = 0;
   for (i = 0; i < this.game.deck[0].hand.length; i++) {
-    if (this.game.deck[0].cards[this.game.deck[0].hand[i]].scoring == 1) { scoring_cards_available++; }
+    if (this.game.deck[0].cards[this.game.deck[0].hand[i]] != undefined) {
+      if (this.game.deck[0].cards[this.game.deck[0].hand[i]].scoring == 1) { scoring_cards_available++; }
+    }
   }
 
 console.log("MOVES REMAINING: " + moves_remaining);
@@ -3323,11 +3325,12 @@ Twilight.prototype.playerPickHeadlineCard = function playerPickHeadlineCard() {
 
   if (this.game.player == 1) { player = "ussr"; }
 
-  let x = player.toUpperCase() + " pick your headline card: <p></p>" + this.returnCardList();
-  this.updateStatus(x);
-  this.addShowCardEvents();
+  let x = player.toUpperCase() + " pick your headline card: <br />" + this.returnCardList(this.game.deck[0].hand);
+
 
   let twilight_self = this;  
+  this.updateStatus(x);
+
 
   $('.card').off();
   twilight_self.addShowCardEvents();
@@ -3406,6 +3409,8 @@ Twilight.prototype.playerTurn = function playerTurn(selected_card=null) {
 
   let player = "ussr";
   let opponent = "us";
+  let playable_cards = [];
+
   if (this.game.player == 2) { player = "us"; opponent = "ussr"; }
 
   is_this_missile_envy_noneventable = this.game.state.events.missileenvy;
@@ -3413,20 +3418,20 @@ Twilight.prototype.playerTurn = function playerTurn(selected_card=null) {
   let user_message = "";
   if (selected_card == null) {
 
-    user_message = player.toUpperCase() + " pick a card: <p></p><ul>";
+    user_message = player.toUpperCase() + " pick a card: ";
     for (i = 0; i < this.game.deck[0].hand.length; i++) {
       //
       // when UN Intervention is eventing, we can only select opponent cards
       //
       if (this.game.state.events.unintervention == 1) {
         if (this.game.player == 1 && this.game.deck[0].cards[this.game.deck[0].hand[i]].player === "us") {
-          user_message += '<li class="card showcard" id="'+this.game.deck[0].hand[i]+'">'+this.game.deck[0].cards[this.game.deck[0].hand[i]].name+'</li>';
+	  playable_cards.push(this.game.deck[0].hand[i]);
         } 
         if (this.game.player == 2 && this.game.deck[0].cards[this.game.deck[0].hand[i]].player === "ussr") {
-          user_message += '<li class="card showcard" id="'+this.game.deck[0].hand[i]+'">'+this.game.deck[0].cards[this.game.deck[0].hand[i]].name+'</li>';
+	  playable_cards.push(this.game.deck[0].hand[i]);
         } 
       } else {
-        user_message += '<li class="card showcard" id="'+this.game.deck[0].hand[i]+'">'+this.game.deck[0].cards[this.game.deck[0].hand[i]].name+'</li>';
+	playable_cards.push(this.game.deck[0].hand[i]);
       }
     };
   } else {
@@ -3436,11 +3441,11 @@ Twilight.prototype.playerTurn = function playerTurn(selected_card=null) {
       for (i = 0; i < this.game.deck[0].hand.length; i++) {
         if (this.game.deck[0].cards[this.game.deck[0].hand[i]].scoring == 1) {
           selected_card = this.game.deck[0].hand[i];
-          user_message += '<li class="card showcard" id="'+selected_card+'">'+this.game.deck[0].cards[selected_card].name+'</li>';
+          playable_cards.push(this.game.deck[0].hand[i]);
         }
       }
     } else {
-      user_message = 'Click to play: <p></p><ul><li class="card showcard" id="'+selected_card+'">'+this.game.deck[0].cards[selected_card].name+'</li>';
+      playable_cards.push(selected_card);
     }
   }
 
@@ -3453,11 +3458,9 @@ Twilight.prototype.playerTurn = function playerTurn(selected_card=null) {
     if (this.game.player == 2 && this.countries['turkey'].us >= 2) { can_remove = 1; }
     if (this.game.player == 2 && this.countries['westgermany'].us >= 2) { can_remove = 1; }
     if (can_remove == 1) {
-      user_message += '<li class="card" id="cancel_cmc">cancel cuban missile crisis</li>';
+      playable_cards.push("cancel cuban missile crisis");
     }
   }
-  user_message += '</ul>';
-
 
   //
   // Bear Trap and Quagmire
@@ -3488,7 +3491,7 @@ Twilight.prototype.playerTurn = function playerTurn(selected_card=null) {
 
     for (i = 0; i < this.game.deck[0].hand.length; i++) {
       if (this.modifyOps(this.game.deck[0].cards[this.game.deck[0].hand[i]].ops) >= 2 && this.game.deck[0].hand[i] != "china") {
-        user_message += '<li class="card showcard" id="'+this.game.deck[0].hand[i]+'">'+this.game.deck[0].cards[this.game.deck[0].hand[i]].name+'</li>';
+        user_message += this.returnCardItem(this.game.deck[0].hand[i]);
         cards_available++;
       }
       if (this.game.deck[0].cards[this.game.deck[0].hand[i]].scoring == 1) { scoring_cards_available++; }
@@ -3504,31 +3507,35 @@ Twilight.prototype.playerTurn = function playerTurn(selected_card=null) {
     } else {
       if (scoring_cards_available > 0) {
         if (this.game.state.events.beartrap == 1) {
-          user_message = "Bear Trap restricts you to Scoring Cards: <p></p><ul>";
+          user_message = "Bear Trap restricts you to Scoring Cards: ";
         } else {
-          user_message = "Quagmire restricts you to Scoring Cards: <p></p><ul>";
+          user_message = "Quagmire restricts you to Scoring Cards: ";
         }
         for (i = 0; i < this.game.deck[0].hand.length; i++) {
           if (this.game.deck[0].cards[this.game.deck[0].hand[i]].scoring == 1) {
-            user_message += '<li class="card showcard" id="'+this.game.deck[0].hand[i]+'">'+this.game.deck[0].cards[this.game.deck[0].hand[i]].name+'</li>';
+            playable_cards(this.game.deck[0].hand[i]);
           }
         }
-        user_message += '</ul>';
-        this.updateStatus(user_message);
       } else {
         if (this.game.state.events.beartrap == 1) {
-          user_message = "No cards playable due to Bear Trap:<p></p><ul>";
+          user_message = "No cards playable due to Bear Trap: ";
         } else {
-          user_message = "No cards playable due to Quagmire:<p></p><ul>";
+          user_message = "No cards playable due to Quagmire: ";
         }
-        user_message += '<li class="card showcard" id="skipturn">skip turn</li>';
-        user_message += '</ul>';
-        this.updateStatus(user_message);
+        playable_cards.push("skip turn");
       }
     }
   }
 
-  this.updateStatus(user_message);
+
+  //
+  // display the cards
+  //
+  if (playable_cards.length > 0) {
+    this.updateStatusAndListCards(user_message, playable_cards);
+  } else {
+    this.updateStatusAndListCards(user_message, this.game.deck[0].hand);
+  }
 
 
   let twilight_self = this;  
@@ -3581,14 +3588,13 @@ Twilight.prototype.playerTurnCardSelected = function playerTurnCardSelected(card
     //
     // Skip Turn
     // 
-    if (card == "skipturn") {
+    if (card === "skipturn") {
       twilight_self.hideCard(card);
       twilight_self.addMove("resolve\tplay");
       twilight_self.addMove("notify\t"+player+" has no cards playable.");
       twilight_self.endTurn();
       return 0;
     }
-
 
 
     //
@@ -3600,7 +3606,9 @@ Twilight.prototype.playerTurnCardSelected = function playerTurnCardSelected(card
     let moves_remaining = rounds_in_turn - twilight_self.game.state.turn_in_round;
 
     for (i = 0; i < twilight_self.game.deck[0].hand.length; i++) {
-      if (twilight_self.game.deck[0].cards[twilight_self.game.deck[0].hand[i]].scoring == 1) { scoring_cards_available++; }
+      if (this.game.deck[0].cards[this.game.deck[0].hand[i]] != undefined) {
+        if (twilight_self.game.deck[0].cards[twilight_self.game.deck[0].hand[i]].scoring == 1) { scoring_cards_available++; }
+      }
     }
 
     if (scoring_cards_available > 0 && scoring_cards_available > moves_remaining && twilight_self.game.deck[0].cards[card].scoring == 0) {
@@ -3631,7 +3639,7 @@ Twilight.prototype.playerTurnCardSelected = function playerTurnCardSelected(card
     //
     // Cuban Missile Crisis
     //
-    if (card == "cancel_cmc") {
+    if (card == "cancelcubanmissilecrisis") {
       if (twilight_self.game.player == 1) {
         twilight_self.removeInfluence("cuba", 2, "ussr");
         twilight_self.addMove("remove\tussr\tussr\tcuba\t2");
@@ -11718,7 +11726,24 @@ Twilight.prototype.doesPlayerDominateRegion = function doesPlayerDominateRegion(
 
 
 
-Twilight.prototype.returnCardList = function returnCardList() {
+Twilight.prototype.returnCardItem = function returnCardItem(card) {
+
+  if (this.interface == 1) {
+    if (this.game.deck[0].cards[card] == undefined) {
+      return `<div id="${card.replace(/ /g,'')}" class="card cardbox-hud cardbox-hud-status">${this.returnCardImage(card)}</div>`;
+    }
+    return `<div id="${card.replace(/ /g,'')}" class="card showcard cardbox-hud cardbox-hud-status">${this.returnCardImage(card)}</div>`;
+  } else {
+    if (this.game.deck[0].cards[card] == undefined) {
+      return '<li class="card" id="'+card+'">'+this.game.deck[0].cards[card].name+'</li>';
+    }
+    return '<li class="card" id="'+card+'">'+this.game.deck[0].cards[card].name+'</li>';
+  }
+
+}
+
+
+Twilight.prototype.returnCardList = function returnCardList(cardarray=[]) {
 
   let hand = this.game.deck[0].hand;
 
@@ -11726,9 +11751,8 @@ Twilight.prototype.returnCardList = function returnCardList() {
 
 
   if (this.interface == 1) {
-
-    for (i = 0; i < this.game.deck[0].hand.length; i++) {
-      html += `<div id="${this.game.deck[0].hand[i]}" class="showcard cardbox-hud cardbox-hud-status">${this.returnCardImage(this.game.deck[0].hand[i])}</div>`;
+    for (i = 0; i < cardarray.length; i++) {
+      html += this.returnCardItem(cardarray[i]);
     }
     html = `
       <div class="cardbox-status-container">
@@ -11740,8 +11764,8 @@ Twilight.prototype.returnCardList = function returnCardList() {
   } else {
 
     html = "<ul>";
-    for (i = 0; i < this.game.deck[0].hand.length; i++) {
-      html += '<li class="card showcard" id="'+this.game.deck[0].hand[i]+'">'+this.game.deck[0].cards[this.game.deck[0].hand[i]].name+'</li>';
+    for (i = 0; i < cardarray.length; i++) {
+      html += this.returnCardItem(cardarray[i]);
     }
     html += '</ul>';
 
@@ -11750,8 +11774,16 @@ Twilight.prototype.returnCardList = function returnCardList() {
   return html;
 
 }
-Twilight.prototype.updateStatusAndListCards = function updateStatusAndListCards(message) {
-  let x = message + "<br />" + this.returnCardList();
+Twilight.prototype.updateStatusAndListCards = function updateStatusAndListCards(message, cards=null) {
+
+  let x = "";
+
+  if (cards == null) {
+    x = message + "<br />" + this.returnCardList(this.game.deck[0].hand);
+  } else {
+    x = message + "<br />" + this.returnCardList(cards);
+  }
+
   this.updateStatus(x);
   this.addShowCardEvents();
 }
@@ -12771,6 +12803,14 @@ Twilight.prototype.returnCardImage = function returnCardImage(cardname) {
   var c = this.game.deck[0].cards[cardname];
   if (c == undefined) { c = this.game.deck[0].discards[cardname]; }
   if (c == undefined) { c = this.game.deck[0].removed[cardname]; }
+  if (c == undefined) {
+
+    //
+    // this is not a card, it is something like "skip turn" or cancel
+    //
+    return '<div class="noncard">'+cardname+'</div>';
+
+  }
 
   var html = `<img class="cardimg" src="/twilight/images/${this.lang}/${c.img}.svg" />`;
 
